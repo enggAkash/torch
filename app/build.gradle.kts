74f3a3ec7e819ecc19c600771fad05290443e956
Akash Kumar <cse.akashkumar@gmail.com>
2025-12-29 23:07:51 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.engineerakash.torch"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
