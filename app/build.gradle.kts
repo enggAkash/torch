2d60839b3cf79dc6be699fd0a25fbc95529a8ff1
Akash Kumar <cse.akashkumar@gmail.com>
2025-12-29 23:01:32 +0530

    removed un used dependecy activity ktx, upgraded java version to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,13 +29,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_1_8
+            jvmTarget = JvmTarget.JVM_11
         }
     }
 }
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.play.services.ads)
 
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.activity.ktx)
 
     testImplementation(libs.junit)
 
